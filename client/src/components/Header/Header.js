7529e8834d2dedd16be8ad98b012e3262422930f
Arvind Shankar <arvind.shankar@ufl.edu>
2019-11-11 14:08:10 -0500

    Logo link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -27,9 +27,12 @@ const Header = () => {
         // </div>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <img src = "/farmLogo.jpg" id="farmLogo"></img>
-            </a>
+            <Link to="/home">
+                <a class="navbar-brand" href="#">
+                    <img src = "/farmLogo.jpg" id="farmLogo"></img>
+                </a>
+            </Link>
+            
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
