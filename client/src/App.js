20d0b9a4a117ddd6a5da1f8d9f89bac87fc25175
Arvind Shankar <arvind.shankar@ufl.edu>
2019-11-27 22:59:18 -0500

    routing for the about page

---
+++ client/src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Route, Switch, Redirect, BrowserRouter  } from 'react-router-dom';
+import NotFound from "./views/NotFound"
+import Header from "./components/Header/Header"
+import Cart from './views/Cart/Cart'
+import Produce from './views/Produce/Produce'
+import Register from './views/Register/Register'
+import AddProduce from './views/AddProduce/AddProduce'
+import About from './views/About/About'
+import axios from 'axios';
+
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  handleRegister = (username, password, confirm_pass) => {
+    // console.log(username);
+    // console.log(password);
+    console.log('Username and Password submitted.');
+    const userData = {
+      username: username,
+      password: password,
+      confirm_pass: confirm_pass
+    };
+    axios.post('http://localhost:5000/api/auth/register', userData)
+      .then(res => console.log(res.data));
+  }
+
+  handleAddProduce = (name, price, farm, description, season) => {
+    console.log('Item name, price, and farm submitted.');
+    const itemData = {
+      name: name,
+      price: price,
+      farm: farm,
+      description: description,
+      season: season
+    };
+    axios.post('http://localhost:5000/api/veggie', itemData)
+      .then(res => console.log(res.data))
+  }
+
+  render(){
+
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/produce" component={Produce} />
+            <Route exact path="/cart" render={(routeProps) => ( <Cart {...routeProps}/> )} />
+            <Route exact path="/register" render={(routeProps) => ( <Register {...routeProps} handleRegister={this.handleRegister} />)} />
+            <Route exact path="/add_produce" render={(routeProps) => (<AddProduce {...routeProps} handleAddProduce={this.handleAddProduce} />)}/>
+            <Route exact path="/">
+              <Redirect to="/produce" />
+            </Route>
+            <Route component={NotFound}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+
+  }
+}
+
+export default App;
