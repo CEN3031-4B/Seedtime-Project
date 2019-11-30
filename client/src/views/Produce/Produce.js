5ff443e82dfde7e89fcbaa19605a956698c49f04
Arvind Shankar <arvind.shankar@ufl.edu>
2019-11-29 21:21:26 -0500

    styling produce page

--- client/src/views/Produce/Produce.js
+++ client/src/views/Produce/Produce.js
@@ -23,55 +23,46 @@ class Produce extends React.Component {
     }
 
     render() {
-
-        // const divStyle = {
-        //     display: 'flex: 1',
-        //     alignItems: 'center',
-        //     justify-content: 'space-between'
-        // };
         
-
         const { veggies} = this.state;
         console.log(veggies);
         const Produce = veggies.map (veggie => {
             return (
-                <div class="row">
-                    <div class="column">
-                        <div className="bags">
-                        <Card id={veggie.id} style={{ width: '18rem' }}>
-                            <Card.Body id="details">
-                                <Card.Title className="title">{veggie.name}</Card.Title>
-                                <Card.Text>
-                                    <div className="price">${veggie.price}</div>
-                                    <br/>
-                                    The Description: {veggie.description}
-                                    <br/>
-                                    Farm: {veggie.farm}
-                                    <br/>
-                                    Season: {veggie.season}
-                                    <br/>                                
-                                </Card.Text>
-                                <Card.Link href="#" onClick = {() => {
-                                    const item = {
-                                        name: veggie.name,
-                                        description: veggie.description,
-                                        season: veggie.season,
-                                        price: veggie.price,
-                                        farm: veggie.farm
-                                    }
-                                    api.insertCartItem(item)
-                                }} className="price_button">Add to Cart</Card.Link>
-                            </Card.Body>
-                        </Card>
-                        </div> 
-                    </div>
-                </div>
+                <div className="bags">
+                <Card id={veggie.id} style={{ width: '18rem' }}>
+                    <Card.Body id="details">
+                        <Card.Title className="title">{veggie.name}</Card.Title>
+                        <Card.Text>
+                            <div className="price">${veggie.price}</div>
+                            <br/>
+                            Description: {veggie.description}
+                            <br/>
+                            Farm: {veggie.farm}
+                            <br/>
+                            Season: {veggie.season}
+                            <br/>                                
+                        </Card.Text>
+                        <Card.Link href="#" onClick = {() => {
+                            const item = {
+                                name: veggie.name,
+                                description: veggie.description,
+                                season: veggie.season,
+                                price: veggie.price,
+                                farm: veggie.farm
+                            }
+                            api.insertCartItem(item)
+                        }} className="price_button">Add to Cart</Card.Link>
+                    </Card.Body>
+                </Card>
+                </div> 
             );
         });
         return (
             <div> 
                 <Alert id="produce-header" variant="success">Welcome to Seedtime Harvest Farms!</Alert>
-                {Produce}
+                <div className="container">
+                    {Produce}
+                </div>
             </div>
         )
         
